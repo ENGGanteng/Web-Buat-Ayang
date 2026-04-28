6cfce9eb1736a5b625e0b9a9738059c1d5bec0dc
Agus Fajar Rahmawan <eng@MacBook-Air-Agus.local>
2026-04-28 22:41:46 +0700

    fix: set tmp path and app key

--- api/index.php
+++ api/index.php
@@ -1,9 +1,8 @@
 <?php
 
-// Menampilkan error jika ada masalah (biar kita tahu salahnya di mana)
+// Paksa menampilkan error buat debugging
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Jembatan utama ke Laravel
+// Jembatan ke Laravel
 require __DIR__ . '/../public/index.php';
