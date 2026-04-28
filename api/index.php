79a27feae5f779e8a5953b6ae610c67743c715d2
Agus Fajar Rahmawan <eng@MacBook-Air-Agus.local>
2026-04-28 23:12:16 +0700

    fix: total bypass storage and cache

--- api/index.php
+++ api/index.php
@@ -1,7 +1,13 @@
 <?php
-// Tampilkan error biar kita tidak menebak
+// 1. Tampilkan error supaya tidak cuma layar putih
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// Jembatan ke folder public
+// 2. Paksa folder-folder bermasalah ke folder sementara (/tmp)
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('SESSION_DRIVER=cookie');
+putenv('LOG_CHANNEL=stderr');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+
 require __DIR__ . '/../public/index.php';
