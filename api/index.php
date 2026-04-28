0adb1b1e4d955816a3a2bfcb631c5400b52c5f14
Agus Fajar Rahmawan <eng@MacBook-Air-Agus.local>
2026-04-28 22:58:29 +0700

    fix: final clean up of bootstrap cache

--- api/index.php
+++ api/index.php
@@ -1,11 +1,18 @@
 <?php
-// Tampilkan error biar kita tidak menebak-nebak
+// 1. Tampilkan error supaya tidak layar putih doang
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// Paksa Laravel menggunakan folder /tmp untuk cache karena folder lain di-lock
-putenv('VIEW_COMPILED_PATH=/tmp');
-putenv('SESSION_DRIVER=cookie');
-putenv('LOG_CHANNEL=stderr');
+// 2. Paksa Laravel pakai folder /tmp (satu-satunya tempat yang bisa nulis di Vercel)
+$tmpPath = '/tmp/storage/framework/';
+$folders = ['views', 'sessions', 'cache'];
+foreach ($folders as $folder) {
+    if (!is_dir($tmpPath . $folder)) {
+        mkdir($tmpPath . $folder, 0777, true);
+    }
+}
 
+putenv("VIEW_COMPILED_PATH=/tmp/storage/framework/views");
+
+// 3. Panggil Laravel
 require __DIR__ . '/../public/index.php';
