d5d4ebb02a2a93d11120645739037f509b30b5ec
Agus Fajar Rahmawan <eng@MacBook-Air-Agus.local>
2026-04-28 22:49:03 +0700

    fix: final attempt with tmp path and debug mode

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
-
-// Paksa menampilkan error buat debugging
+// Tampilkan error secara detail
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Jembatan ke Laravel
