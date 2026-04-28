425748b80d95e2049914dbde6fcfd73f708bf96d
Agus Fajar Rahmawan <eng@MacBook-Air-Agus.local>
2026-04-28 23:54:12 +0700

    fix: bersihkan cache dan lock terakhir

--- api/index.php
+++ api/index.php
@@ -1,10 +1,11 @@
 <?php
 
-// Tampilkan error jika ada masalah
+// Tampilkan error biar kita tahu masalah aslinya kalau gagal
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// Paksa path storage ke folder /tmp (hanya folder ini yang bisa ditulis di Vercel)
-putenv('VIEW_COMPILED_PATH=/tmp');
+// Paksa Laravel abaikan config cache
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
 
 require __DIR__ . '/../public/index.php';
