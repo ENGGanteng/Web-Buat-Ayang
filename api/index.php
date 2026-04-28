b7260281582b1c4ae39d34ad34b3c18b0a0b8bad
Agus Fajar Rahmawan <eng@MacBook-Air-Agus.local>
2026-04-28 22:52:27 +0700

    fix: last hope for laravel config

--- api/index.php
+++ api/index.php
@@ -1,8 +1,11 @@
 <?php
-// Tampilkan error secara detail
+// Tampilkan error biar kita tidak menebak-nebak
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Jembatan ke Laravel
+// Paksa Laravel menggunakan folder /tmp untuk cache karena folder lain di-lock
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('SESSION_DRIVER=cookie');
+putenv('LOG_CHANNEL=stderr');
+
 require __DIR__ . '/../public/index.php';
